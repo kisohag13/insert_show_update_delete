08b64aea37c234895d2ea6a878d9c217abd72402
Md. Kamrul Islam <ki.sohag@gmail.com>
2016-02-25 19:53:58 +0600

    it's index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset ="UTF-8">
+<title>Database connection project</title>
+</head>
+<body>
+<h2>secect your option</h2>
+
+<ul>
+	<li><a href="insert.php">Insert data</li>
+	<li><a href="show.php">Show data</li>
+</ul>
+
+
+</body>
+</html>
